87b952ebd79759c5b0714432ece52359cce573fb
unknown <roc0004@alu.ubu.es>
2018-02-27 12:10:05 +0100

    Imagen Ball.png
    Texto HECHO

--- src/Ball.java
+++ src/Ball.java
@@ -2,7 +2,8 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 //TODO Transform the code to be used safely in a concurrent context.  
 public class Ball {
-       //TODO  Find an archive named Ball.png 
+    //TODO  Find an archive named Ball.png
+	//HECHO Incluido el archivo Ball.png
 	private String Ball = "Ball.png"; 
 
 	private double x,y,dx,dy;
